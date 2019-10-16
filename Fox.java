58e917b49151b959a3e661fdfc81476938e6e924
AshtonMohns <55707228+AshtonMohns@users.noreply.github.com>
2019-10-16 12:30:08 -0400

    Update Fox.java

--- Fox.java
+++ Fox.java
@@ -1,21 +1,16 @@
 
 public class Fox extends GamePiece {
-	private String name;
 	private boolean upDown;
 	
 	public Fox(String s, int xpos, int ypos, boolean direction) {
-		super(xpos, ypos, 2);
-		name = s;
+		super(xpos, ypos, 2, s);
 		upDown = direction;	
 	}
 	
 	public boolean getUpDown() {
 		return upDown;
 	}
-	@Override
-	public String getName(){
-		return name;
-	}
+
 	public void move() {
 		
 	}
