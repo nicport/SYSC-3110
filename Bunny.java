c4e1b64b5ea560b00ffb69652dc6044622030c89
AshtonMohns <55707228+AshtonMohns@users.noreply.github.com>
2019-10-16 12:29:25 -0400

    Update Bunny.java

--- Bunny.java
+++ Bunny.java
@@ -1,16 +1,10 @@
 
 public class Bunny extends GamePiece {
-	private String name;
 	
 	public Bunny(String s, int xpos, int ypos) {
-		super(xpos, ypos, 1);
-		this.name = s;
+		super(xpos, ypos, 1, s);
 	}
 	public void move() {
 		
-	}
-	@Override
-	public String getName(){
-		return name;
 	}
 }
