2f17765cd69f58d13b70961e5c870579c6252736
AshtonMohns <55707228+AshtonMohns@users.noreply.github.com>
2019-10-16 12:25:48 -0400

    Update Mushroom.java

--- Mushroom.java
+++ Mushroom.java
@@ -3,16 +3,14 @@ public class Mushroom extends GamePiece {
 	private String name;
 	
 	public Mushroom(String s, int xpos, int ypos) {
+		super(xpos, ypos, 1);
 		name = s;
-		x = xpos;
-		y = ypos;
-		fox = false;
-		bunny = false;
-		mushroom = true;
 	}
+	
 	public void move() {
 		
 	}
+	
 	@Override
 	public String getName(){
 		return name;
